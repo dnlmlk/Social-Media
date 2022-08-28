6acf24a8a248eb4352d5b8f7d77b85b545587887
dnlmlk <malekiandan@gmail.com>
2022-08-29 02:13:55 +0430

    README

--- resources/views/firstPage.blade.php
+++ resources/views/firstPage.blade.php
@@ -4,7 +4,7 @@
     <!--
     More Templates Visit ==> Free-Template.co
     -->
-    <title>Main Page</title>
+    <title>Social Media</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="Free Template by Free-Template.co" />
